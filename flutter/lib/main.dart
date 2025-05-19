86f784abc29a27a492509cd7b55eb640030cc21b
Mohamed Abdelaal <mabdelaal86@gmail.com>
2025-05-19 14:45:35 +0200

    Fix screen config issue
    Set screen configuration on android (<15) devices to fix the issue with status bar.

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,8 +1,23 @@
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
 import 'controllers/game.dart';
 
-void main() {
+Future<void> main() async {
+  await configureDeviceScreen();
   runApp(GameWidget(game: DjambiGame()));
+}
+
+Future<void> configureDeviceScreen() async {
+  const transparentColor = Color(0x00000000);
+  SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+          statusBarColor: transparentColor,
+          systemNavigationBarColor: transparentColor,
+      ));
+  WidgetsFlutterBinding.ensureInitialized();
+  await Flame.device.restoreFullscreen();
+  await Flame.device.setPortrait();
 }
